269602f0388d3ff3d0104daa3cc0d7f2ec3b5f50
Sijibomi962 <adekoyasijibomi256@gmail.com>
2023-03-23 21:03:06 +0100

    First Commit

---
+++ Static/js/script.js
@@ -0,0 +1,24 @@
+// const changeImg = document.querySelector(".change-img");
+// let changed = changeImg.src
+
+
+// changeImg.addEventListener('mouseover', function(){
+//     changeImg.src = 'static/img/text20.jpeg';
+// })
+// changeImg.addEventListener('mouseleave', function(){
+//     changeImg.src = changed;
+    
+// })
+
+
+//  const changeImg = document.querySelectorAll(".change-img");
+
+//  for(let i = 0; i < changeImg.length; i++){
+//  let changed = changeImg[i].src
+//     changeImg[i].addEventListener('mouseover', function(){
+//         changeImg[i].src = `static/img/text${21-i}.jpeg`
+//     })
+//     changeImg[i].addEventListener('mouseleave', function(){
+//  changeImg[i].src = changed;
+//     })
+//  }
